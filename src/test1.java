c4ed1f8812625a0360276557f70418a5495459d0
yumna-nizar <nizarrasheeda270@gmail.com>
2026-07-03 21:27:39 +0530

    wrap around concept with minvalue

--- src/test1.java
+++ src/test1.java
@@ -1,11 +1,14 @@
 public class test1 {
 
-    int rollno=2147483647;
+    int MAXValue=2147483647;
+    int MINValue=-2147483648;
 
      void display()
     {
-        rollno=2147483647+1;//overflow condition
-        System.out.println(rollno);
+        MAXValue=MAXValue+1;//overflow condition
+        MINValue=MINValue-1;
+        System.out.println(MAXValue);
+        System.out.println(MINValue);
     }
     public static void main(String[] args)
     {
