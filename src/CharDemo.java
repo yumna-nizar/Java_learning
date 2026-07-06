f1db6c28210db74faf96b8da6680c7ffcc1ecfdc
yumna-nizar <nizarrasheeda270@gmail.com>
2026-07-06 11:04:43 +0530

    printing maxvalue and minvalue of char datatype

--- src/CharDemo.java
+++ src/CharDemo.java
@@ -2,11 +2,19 @@ public class CharDemo {
     char ch1='A';
     char ch2='\u0041';
     char ch3=65;
+    char maxval=Character.MAX_VALUE;
+    char minval=Character.MIN_VALUE;
+    int a=(int)maxval;
+    int b=(int)minval;
     void display()
     {
-        System.out.println(ch1);
-        System.out.println(ch2);
-        System.out.println(ch3);
+        System.out.println(ch1);//A
+        System.out.println(ch2);//A
+        System.out.println(ch3); //A
+        System.out.println(minval);//￿
+        System.out.println(maxval); //￿
+        System.out.println(a); //65535
+        System.out.println(b); //0
     }
     public static void main(String[] args)
     {
