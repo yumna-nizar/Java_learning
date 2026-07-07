85c7756f1b36304700528a32f8c2bbddb1c0729b
yumna-nizar <nizarrasheeda270@gmail.com>
2026-07-07 14:02:29 +0530

    escape sequence example

---
+++ src/EscapeSequence.java
@@ -0,0 +1,12 @@
+public class EscapeSequence {
+  public static void main() {
+
+      System.out.println("hello yumna\b welocome");//backspace
+      System.out.println("hello yusra\r welcome");//carraige return
+      System.out.println("hello \'yumna\'");//single quote
+      System.out.println("hello \"yumna\"");//double quote
+      System.out.println("hello\tyumna");//4 spaces (tab)
+      System.out.println("hello\nyumna");//newline
+      System.out.println("hello\\yumna\\");//backlash
+    }
+}
