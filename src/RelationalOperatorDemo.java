498d91427a315aea121c46c997a907fde7a8888d
yumna-nizar <nizarrasheeda270@gmail.com>
2026-07-08 15:09:47 +0530

    comparison operator with string created with two different methods eg

---
+++ src/RelationalOperatorDemo.java
@@ -0,0 +1,11 @@
+public class RelationalOperatorDemo {
+     public static void main(String[] args) {
+        String a="yumna";
+        String b="yumna";
+        System.out.println(a==b);
+        String c=new String("yusra");
+        String d=new String("yusra");
+        System.out.println(c==d);
+    }
+
+}
---
+++ src/ScannerClassDemo.java
@@ -0,0 +1,44 @@
+
+
+import java.lang.invoke.StringConcatFactory;
+import java.util.Scanner;
+
+public class ScannerClassDemo {
+
+    public static void main(String[] args)
+    {
+//        int a=2023;
+//        int b=20;
+//        char ch1='A',ch2='B';
+//        ch2/=2;
+//        System.out.println(ch2);
+//        int a=10,b=20;
+//        int result =a + b - b++ + ++a - a++ * ++b;
+//        System.out.println(result);
+//        String a="hello";
+//        String b=a+" yumna";
+//        System.out.println(b);
+//        int a=7;
+//        int b=a+3*6;
+//        int c=a;
+//        System.out.println(b);
+
+
+
+
+
+//        System.out.println(ch1);
+//        System.out.println(ch1+ch2);
+//        System.out.println(ch1-ch2);
+//        System.out.println(ch2/ch1);
+//        System.out.println(ch2%ch1);
+//        System.out.println(""+ch1+ch2);
+
+
+
+
+
+
+    }
+
+}
