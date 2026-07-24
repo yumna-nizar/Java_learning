909f2f9026b4aa23bd3afcfec4bfa7b7bcade746
yumna-nizar <nizarrasheeda270@gmail.com>
2026-07-24 14:03:03 +0530

    pattern printing 1

---
+++ src/PrintPattern.java
@@ -0,0 +1,50 @@
+public class PrintPattern {
+    static void main() {
+//pattern 1
+//        int rows,cols;
+//        rows=cols=5;
+//        for(int i=rows;i>=1;i--)            //    *****
+//        {                                   //    ****
+//            for(int j=1;j<=i;j++)           //    ***
+//            {                               //    **
+//                System.out.print("*");      //    *
+//            }
+//            System.out.println(" ");
+//        }
+
+
+
+//pattern 2
+//        int rows=5,cols=5;
+//        for(int i=rows;i>=1;i--)
+//        {                                     //      *
+//            for(int j=1;j<=cols;j++)          //     **
+//            {                                 //    ***
+//                if(i-j<1)                     //   ****
+//                {                             //  *****
+//                    System.out.print("*");
+//                }
+//                else {
+//                    System.out.print(" ");
+//                }
+//
+//
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+//pattern 3
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=i;j++)
+//            {
+//
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+    }
+}
